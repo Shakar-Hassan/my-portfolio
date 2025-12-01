28c84a5436810d4ac99a12330fb66a511381b9f1
Shakar Hassan <shakarmohammed2002@gmail.com>
2025-12-01 20:22:54 +0100

    project

---
+++ src/AppComponents/Sections/Projects.jsx
@@ -0,0 +1,28 @@
+import AnimatedSection from "../UI/AnimatedSection";
+import ProjectCard from "../UI/ProjectCard";
+
+export default function Projects() {
+  return (
+    <section id="projects">
+      <AnimatedSection title="Featured Projects" delay={0.2}>
+        <div className="projects-grid">
+          <ProjectCard
+            title="Lingo Live"
+            desc="An Interactive Web Application for German Language Learning Progress Tracking."
+            tags={["JavaScript", "Ruby on rails", "PostgreSQL"]}
+          />
+          <ProjectCard
+            title="Eco Bickers"
+            desc="Sustainable Transport Web Platform for Eco-friendly Products."
+            tags={["JavaScript", "Ruby on rails", "PostgreSQL"]}
+          />
+          <ProjectCard
+            title="NicBlendz"
+            desc="Custom Web Platform for Barber Shop Operations and Client Management."
+            tags={["JavaScript", "Ruby on rails", "PostgreSQL"]}
+          />
+        </div>
+      </AnimatedSection>
+    </section>
+  );
+}
